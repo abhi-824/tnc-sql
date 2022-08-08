a40524bed0181476f010542ae8e2760131f18f69
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2022-08-08 18:36:58 +0530

    init.js

--- init.js
+++ init.js
@@ -1,9 +1,24 @@
-var pg = require('pg');
+require("dotenv").config();
+const Pool = require("pg").Pool;
+
+const url = require("url");
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(":");
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+};
+
+const pool = new Pool(config);
+
+
 var fs = require('fs');
 
 var sql = fs.readFileSync('init.sql').toString();
-
-pg.connect(process.env.DATABASE_URL, function(err, client, done){
+pool.connect(function(err, client, done){
     if(err){
         console.log('error: ', err);
         process.exit(1);
--- pool.js
+++ pool.js
@@ -5,15 +5,15 @@ const Pool = require("pg").Pool;
 const url = require("url");
 const params = url.parse(process.env.DATABASE_URL);
 const auth = params.auth.split(":");
-const config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split("/")[1],
-};
+    const config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split("/")[1],
+    };
 
-const pool = new Pool(config);
+    const pool = new Pool(config);
 
 
 module.exports = {
